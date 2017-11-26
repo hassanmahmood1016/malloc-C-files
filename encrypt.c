8029acc56a2a9038390f4d378f5abdba1a4c91ce
hassanmahmood1016 <33792700+hassanmahmood1016@users.noreply.github.com>
2017-11-25 19:56:28 -0500

    Create encrypt.c

---
+++ encrypt.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+#include <string.h>
+
+#include <stdlib.h>
+
+
+
+
+char* encryptDecrypt(char* name, char key, int size)
+
+{
+
+	char* ename = malloc(size + 1);
+
+
+
+
+	ename[size] = '\0';
+
+	int i;
+
+	for (i = 0; i < size; i++)
+
+	{
+
+		ename[i] = key^name[i];
+
+	}
+
+
+
+
+	return ename;
+
+}
+
+
+
+
+int main()
+
+{
+
+	char key;
+
+	printf("Please enter a key: ");
+
+	key = getchar();
+
+
+
+
+	char* name = "hassanmahmood";
+
+	char* ename = encryptDecrypt(name, key, 13);
+
+	char* dname = encryptDecrypt(ename, key, 13);
+
+
+
+
+	printf("Raw string : %s\n", name);
+
+
+
+
+	printf("Encrypted : %s\n", ename);
+
+
+
+
+	printf("Decrypted : %s\n", dname);
+
+
+
+
+	return 0;
+
+}
